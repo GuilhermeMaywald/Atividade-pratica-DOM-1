fb362b6ba0da6dababf5194f4529013e2213b35a
guilherme maywald <guilhermemaywald@gmail.com>
2022-09-20 08:33:03 -0300

    Primeira atividade pratica com DOM

---
+++ js/dom_1.js
@@ -0,0 +1,8 @@
+
+let paragrafos = document.getElementsByTagName("p");
+console.log(paragrafos[0].innerText);
+console.log(paragrafos[1].innerText);
+console.log(paragrafos[2].innerText);
+console.log(paragrafos[3].innerText);
+console.log(paragrafos[4].innerText);
+
